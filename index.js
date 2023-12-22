3c4c1e676508aad59c96d71225c2fd5ca2b289ab
Shariar <shariarnafis86@gmail.com>
2023-12-22 19:10:44 +0600

    delete a task

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors())
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pbcjxfk.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -34,17 +34,19 @@ async function run() {
       res.send(result)
     })
 
-    app.get('/createTask', async (req, res) => {
+    app.get('/myTask', async (req, res) => {
       const email = req.query.email;
       const query = { email: email }
       const result = await createTask.find(query).toArray()
       res.send(result)
     })
 
-    // app.get('/createTask', async (req, res) => {
-    //   const result = await createTask.find().toArray()
-    //   res.send(result)
-    // })
+    app.delete('/myTask/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await createTask.deleteOne(query)
+      res.send(result)
+  })
 
     await client.connect();
     // Send a ping to confirm a successful connection
